d5a1e1aa0e33efd98447d346b648fde58e998f18
HISASHI TAKAGI <105723230+hisashi-takagi0925@users.noreply.github.com>
2023-03-25 21:23:36 +0900

    Update models.ts

--- src/DDD/Repository/Dmm/models.ts
+++ src/DDD/Repository/Dmm/models.ts
@@ -1,5 +1,5 @@
-export const API_KEY: string = "kcyJ3aTeF7x4z40KqByL";
-export const AFFILIATE_ID: string = "emovier0925-990";
+export const API_KEY: string = "";
+export const AFFILIATE_ID: string = "";
 
 export const ACTRESS_SEARCH_BASE_URL: string =
   "https://api.dmm.com/affiliate/v3/ActressSearch";
